71766baa7127b4dfcd52042fd7eb4b09526cd0d1
huzaifa <107106387+huzaifaah@users.noreply.github.com>
2022-06-11 10:36:10 +0500

    MuhammadHuzaifa

---
+++ Literals.java
@@ -0,0 +1,26 @@
+public class Literals
+{
+	public static void main(String args[])
+	{
+		int a=70;
+		System.out.println(a);
+		int b=064;
+		System.out.println(b);
+		int c=0x23;
+		System.out.println(c);
+		float d=3.1415926f;
+		System.out.println(d);
+		double e=3.14159260;
+		System.out.println(e);
+		double f=3.1415e7;
+		System.out.println(f);
+		double g=0x3.Ep1;
+		System.out.println(g);
+		char h=65;
+		System.out.println(h);
+		char i='\101';
+		System.out.println(i);
+		char j='\u0044';
+		System.out.println(j);
+	}
+}
